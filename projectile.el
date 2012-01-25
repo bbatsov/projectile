cdcf5afc65dc916a836010f0b41613f3b1e264fc
Fabian Otto <fabian.otto@familienservice.de>
2012-01-25 13:39:38 +0100

    Adds suport for anything
    - add a simple wrapper for generating a list of files from the current
      project
    - defines a anything source definition for projectile
    - example function for calling anything

--- projectile.el
+++ projectile.el
@@ -225,6 +225,28 @@
      ["Invalidate cache" projectile-invalidate-project-cache]
      ["Regenerate etags" projectile-regenerate-tags])))
 
+;; helper for anything
+(defun anything-c-projectile-list ()
+  "Generates a list of files in the current project"
+  (projectile-get-project-files
+   (projectile-get-project-root)))
+
+(defvar anything-c-source-projectile-list
+  `((name . "Projectile Files")
+    (candidates . anything-c-projectile-list)
+    (type . file)
+    (mode-line . "Projectile Files")
+    )
+  "Anything source definition")
+
+(defun anything-with-projectile-list ()
+  "Example function for calling anything with the projectile file source.
+
+Use this function as example and create your own list of anything sources.
+"
+  (interactive)
+  (anything :sources '(anything-c-source-projectile-list)))
+
 ;; define minor mode
 (define-globalized-minor-mode projectile-global-mode projectile-mode projectile-on)
 
