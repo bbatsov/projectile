982023b427ec936ecf4643ec312664051ed35cd3
Bozhidar Batsov <bozhidar@batsov.com>
2012-04-27 16:10:59 +0300

    added a couple of tests

--- projectile-tests.el
+++ projectile-tests.el
@@ -39,6 +39,13 @@
 
 (require 'projectile)
 
+(ert-deftest projectile-test-string-suffix-p ()
+  (should (projectile-string-suffix-p "test.txt" "txt"))
+  (should (not (projectile-string-suffix-p "test.txt" "rb"))))
+
+(ert-deftest projectile-test-uniquify-file ()
+  (should (equal (uniquify-file "ala/bala/portokala") "bala/portokala")))
+
 (ert-deftest projectile-test-project-get-name ()
   (should (equal (projectile-get-project-name) "projectile")))
 
