58a84ec76ef96c6346e649d7d754499ca7b74fa9
Niels Widger <niels.widger@gmail.com>
2013-12-12 15:53:30 -0500

    Modified `projecile-ack` to append options to `ack-and-a-half-arguments` instead of overriding them.

--- projectile.el
+++ projectile.el
@@ -928,11 +928,13 @@ With a prefix ARG invalidates the cache first."
 (defun projectile-ack ()
   "Run an `ack-and-a-half' search in the project."
   (interactive)
-  (let ((ack-and-a-half-arguments
-         (-map
-          (lambda (path)
-            (concat "--ignore-dir=" (file-name-nondirectory (directory-file-name path))))
-          (projectile-ignored-directories))))
+  (let* ((saved-arguments ack-and-a-half-arguments)
+         (ack-and-a-half-arguments
+          (append saved-arguments
+                  (-map
+                   (lambda (path)
+                     (concat "--ignore-dir=" (file-name-nondirectory (directory-file-name path))))
+                   (projectile-ignored-directories)))))
     (call-interactively projectile-ack-function)))
 
 (defun projectile-ag (regexp)
