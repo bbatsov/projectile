37a5c160a221407f330d0d518a408892735487a9
Aaron Ecay <aaronecay@gmail.com>
2013-12-13 15:24:02 -0500

    allow projectile-project-root to find the root for symlinked files
    This covers the case where you have a git repo ~/configs (for dotfiles
    etc.), and the subdir emacs of that repo is symlinked to ~/.emacs.d
    
    projectile-project-root in a buffer visiting ~/.emacs.d/init.el will
    find the root at ~/configs.
    
    It also covers the case of a single file symlinked outside a repo.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@
 
 * Added new `projectile-commander` methods ?v and ?R which run
   `projectile-vc-dir` and `projectile-regenerate-tags`, respectively.
+* Projectile is now able to find the project pertaining to a symlink
+  pointing to a version-controlled file.
 
 ## 0.10.0 (12/09/2013)
 
--- projectile.el
+++ projectile.el
@@ -346,7 +346,9 @@ The cache is created both in memory and on the hard drive."
 The current directory is assumed to be the project's root otherwise."
   (let ((project-root
          (or (->> projectile-project-root-files
-               (--map (locate-dominating-file default-directory it))
+               (--map (locate-dominating-file (file-truename
+                                               (or buffer-file-name default-directory))
+                                              it))
                (-remove #'null)
                (car)
                (projectile-file-truename))
