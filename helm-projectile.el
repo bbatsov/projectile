d5ceb07541d791cec4438993d98915340f3def4f
Bozhidar Batsov <bozhidar@batsov.com>
2014-02-25 19:59:24 +0200

    [Fix #277] Correct the cl-lib dep in helm-projectile

--- helm-projectile.el
+++ helm-projectile.el
@@ -7,7 +7,7 @@
 ;; Created: 2011-31-07
 ;; Keywords: project, convenience
 ;; Version: 0.10.0
-;; Package-Requires: ((helm "1.4.0") (projectile "0.10.0") (cl-lib 0.3))
+;; Package-Requires: ((helm "1.4.0") (projectile "0.10.0") (cl-lib "0.3"))
 
 ;; This file is NOT part of GNU Emacs.
 
