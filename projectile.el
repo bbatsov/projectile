002bddf99f2013fee4a976ea463a854344df7208
Daniel Mijares <daniel.j.mijares@gmail.com>
2014-11-08 23:13:10 -0500

    git cannot run submodule if the .git folder is not at the project root folder.
    projectile-vcs-root determines a project is git traversing up the path
    until a `.git` folder is found.  This is fine up until the submodule
    command.
    
    If you run git submodule --quiet on a sub folder, you get
    
    "You need to run this command from the toplevel of the working tree."
    
    This can be problematic for projects that have a projectile-project-root
    that is a child directory of its actual repository root.

--- projectile.el
+++ projectile.el
@@ -781,9 +781,12 @@ Files are returned as relative paths to the project root."
      (t projectile-generic-command))))
 
 (defun projectile-get-sub-projects-command ()
-  (let ((vcs (projectile-project-vcs)))
+  (let ((vcs (projectile-project-vcs))
+        (project-root (projectile-project-root)))
     (cond
-     ((eq vcs 'git) projectile-git-submodule-command)
+     ((projectile-file-exists-p (expand-file-name ".git" project-root))
+      (cond (eq vcs 'git)
+          projectile-git-submodule-command))
      (t ""))))
 
 (defun projectile-get-all-sub-projects (project &optional known-projects)
