700d1129a77f0bdb863d6e25a8ad522d008cec80
Bozhidar Batsov <bozhidar@batsov.com>
2016-02-28 10:08:55 +0200

    Bump the development version

--- projectile.el
+++ projectile.el
@@ -5,7 +5,7 @@
 ;; Author: Bozhidar Batsov <bozhidar@batsov.com>
 ;; URL: https://github.com/bbatsov/projectile
 ;; Keywords: project, convenience
-;; Version: 0.13.0
+;; Version: 0.14.0-cvs
 ;; Package-Requires: ((dash "2.11.0") (pkg-info "0.4"))
 
 ;; This file is NOT part of GNU Emacs.
