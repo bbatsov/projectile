2d43f719a4300944119f90f2e32d57887f4769ae
Bozhidar Batsov <bozhidar@batsov.com>
2016-10-05 15:21:55 +0300

    Fix indentation and add missing metadata

--- test/projectile-test.el
+++ test/projectile-test.el
@@ -684,29 +684,31 @@
 
 (ert-deftest projectile-test-exclude-out-of-project-submodules ()
   (projectile-test-with-files
-      (;; VSC root is here
-       "project/"
-       "project/.git/"
-       "project/.gitmodules"
-       ;; Current project root is here:
-       "project/web-ui/"
-       "project/web-ui/.projectile"
-       ;; VCS git submodule will return the following submodules,
-       ;; relative to current project root, 'project/web-ui/':
-       "project/web-ui/vendor/client-submodule/"
-       "project/server/vendor/server-submodule/")
-    (let ((project (file-truename (expand-file-name "project/web-ui"))))
-      (noflet ((projectile-files-via-ext-command
-                (arg) (when (string= default-directory project)
-                        '("vendor/client-submodule"
-                          "../server/vendor/server-submodule")))
-               (projectile-project-root
-                () project))
+   (;; VSC root is here
+    "project/"
+    "project/.git/"
+    "project/.gitmodules"
+    ;; Current project root is here:
+    "project/web-ui/"
+    "project/web-ui/.projectile"
+    ;; VCS git submodule will return the following submodules,
+    ;; relative to current project root, 'project/web-ui/':
+    "project/web-ui/vendor/client-submodule/"
+    "project/server/vendor/server-submodule/")
+   (let ((project (file-truename (expand-file-name "project/web-ui"))))
+     (noflet ((projectile-files-via-ext-command
+               (arg) (when (string= default-directory project)
+                       '("vendor/client-submodule"
+                         "../server/vendor/server-submodule")))
+              (projectile-project-root
+               () project))
 
-        ;; assert that it only returns the submodule 'project/web-ui/vendor/client-submodule/'
-        (should (equal (list (expand-file-name "vendor/client-submodule/" project))
-                       (projectile-get-all-sub-projects project)))))))
+       ;; assert that it only returns the submodule 'project/web-ui/vendor/client-submodule/'
+       (should (equal (list (expand-file-name "vendor/client-submodule/" project))
+                      (projectile-get-all-sub-projects project)))))))
 
 ;; Local Variables:
 ;; indent-tabs-mode: nil
 ;; End:
+
+;;; projectile-test.el ends here
