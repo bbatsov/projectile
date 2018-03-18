4fae48d12662f3da30b97731f5b91c6567e19e27
Jorge Araya Navarro <jorge@esavara.cr>
2018-03-17 18:51:15 -0600

    Call function instead of using variable
    The function was incorrectly defined, causing to fail on tests. The correct thing to do is to call
    `projectile-ignored-directories` which returns a sequence.

--- projectile.el
+++ projectile.el
@@ -1526,7 +1526,7 @@ projectile project root."
   (cl-some
    (lambda (name)
      (string-match-p name directory))
-   projectile-globally-ignored-directories))
+   (projectile-ignored-directories)))
 
 (defun projectile-ignored-file-p (file)
   "Check if FILE should be ignored."
