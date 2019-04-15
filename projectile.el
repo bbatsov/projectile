90eb5bb35a2c87f111f97c5cc4e9071a97eafaf1
David Strawn <isomarcte@gmail.com>
2019-04-15 11:43:37 -0600

    Update Bloop Test Configuration
    This test configuration will by default run _all_ tests for Bloop/SBT projects which follow the standard idioms.

--- projectile.el
+++ projectile.el
@@ -2652,7 +2652,7 @@ test/impl/other files as below:
                                   :test-suffix "_test")
 (projectile-register-project-type 'bloop '(".bloop")
                                   :compile "bloop compile root"
-                                  :test "bloop test root")
+                                  :test "bloop test --propagate --reporter scalac root")
 ;; Ruby
 (projectile-register-project-type 'ruby-rspec '("Gemfile" "lib" "spec")
                                   :compile "bundle exec rake"
