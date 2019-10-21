0d3281f542444e3dbdb13d661a1f09530397ae5a
riscy <riscy@users.noreply.github.com>
2019-10-20 18:44:13 -0700

    Sharp-quote function references

--- projectile.el
+++ projectile.el
@@ -1168,7 +1168,7 @@ Files are returned as relative paths to DIRECTORY."
 The function calls itself recursively until all sub-directories
 have been indexed.  The PROGRESS-REPORTER is updated while the
 function is executing."
-  (apply 'append
+  (apply #'append
          (mapcar
           (lambda (f)
             (unless (or (and patterns (projectile-ignored-rel-p f directory patterns))
@@ -2938,7 +2938,7 @@ Fallback to DEFAULT-VALUE for missing attributes."
   (let ((grouped-candidates (projectile-group-file-candidates file candidates)))
     (if (= (length (car grouped-candidates)) 2)
         (list (car (last (car grouped-candidates))))
-      (apply 'append (mapcar 'cdr grouped-candidates)))))
+      (apply #'append (mapcar #'cdr grouped-candidates)))))
 
 (defun projectile--impl-to-test-predicate (impl-file)
   "Return a predicate, which returns t for any test files for IMPL-FILE."
@@ -3063,7 +3063,7 @@ which it shares its arglist."
                  ;; we should use shell-quote-argument here
                  " -path "
                  (mapconcat
-                  'identity
+                  #'identity
                   (delq nil (mapcar
                              #'(lambda (ignore)
                                  (cond ((stringp ignore)
@@ -3337,21 +3337,21 @@ regular expression."
   "Invoke `execute-extended-command' in the project's root."
   (interactive)
   (projectile-with-default-dir (projectile-ensure-project (projectile-project-root))
-    (call-interactively 'execute-extended-command)))
+    (call-interactively #'execute-extended-command)))
 
 ;;;###autoload
 (defun projectile-run-shell-command-in-root ()
   "Invoke `shell-command' in the project's root."
   (interactive)
   (projectile-with-default-dir (projectile-ensure-project (projectile-project-root))
-    (call-interactively 'shell-command)))
+    (call-interactively #'shell-command)))
 
 ;;;###autoload
 (defun projectile-run-async-shell-command-in-root ()
   "Invoke `async-shell-command' in the project's root."
   (interactive)
   (projectile-with-default-dir (projectile-ensure-project (projectile-project-root))
-    (call-interactively 'async-shell-command)))
+    (call-interactively #'async-shell-command)))
 
 ;;;###autoload
 (defun projectile-run-shell ()
@@ -4343,7 +4343,7 @@ is chosen."
 
   (def-projectile-commander-method ?a
     "Run ag on project."
-    (call-interactively 'projectile-ag))
+    (call-interactively #'projectile-ag))
 
   (def-projectile-commander-method ?s
     "Switch project."
