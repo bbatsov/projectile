6e6b060a52614dcab931f0f670ebbd93c44944ca
Zhongwei Yao <ashi08104@gmail.com>
2020-02-28 18:25:06 +0800

    [Fix #705] let tag generation be aware of ignore pattern.

--- projectile.el
+++ projectile.el
@@ -3609,7 +3609,9 @@ regular expression."
   "Return a string with exclude patterns for ctags."
   (mapconcat (lambda (pattern) (format "--exclude=\"%s\""
                                        (directory-file-name pattern)))
-             (projectile-ignored-directories-rel) " "))
+             (append
+              (projectile-ignored-directories-rel)
+              (projectile-patterns-to-ignore)) " "))
 
 ;;;###autoload
 (defun projectile-regenerate-tags ()
