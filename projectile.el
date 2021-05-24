9d07837cfa2b6afef33172f5c5b0b6788561d052
Giap Tran <txgvnn@gmail.com>
2021-05-24 22:54:53 +0700

    Add savehist-mode comment

--- projectile.el
+++ projectile.el
@@ -5125,6 +5125,8 @@ Otherwise behave as if called interactively.
     (advice-remove 'compilation-find-file #'compilation-find-file-projectile-find-compilation-buffer)
     (advice-remove 'delete-file #'delete-file-projectile-remove-from-cache))))
 
+;;; savehist-mode - When `savehist-mode' is t, projectile-project-command-history will be saved.
+;; See https://github.com/bbatsov/projectile/issues/1637 for more details
 (if (bound-and-true-p savehist-loaded)
     (add-to-list 'savehist-additional-variables 'projectile-project-command-history)
   (defvar savehist-additional-variables nil)
