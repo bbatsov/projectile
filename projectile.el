0404b9b6eeb0c6504337749a366fffd97de2bfbe
sencrash <scrash@tuta.io>
2024-12-25 21:04:58 +0000

    Replace jj files with jj file list

--- projectile.el
+++ projectile.el
@@ -725,7 +725,7 @@ Set to nil to disable listing submodules contents."
   :group 'projectile
   :type 'string)
 
-(defcustom projectile-jj-command "jj files --no-pager . | tr '\\n' '\\0'"
+(defcustom projectile-jj-command "jj file list --no-pager . | tr '\\n' '\\0'"
   "Command used by projectile to get the files in a Jujutsu project."
   :group 'projectile
   :type 'string
