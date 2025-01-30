07fc5ebc482af295211f6e3df91491ca4a89ea70
Bozhidar Batsov <bozhidar@batsov.dev>
2025-01-30 09:47:25 +0200

    Fix a couple of broken tests

--- test/projectile-test.el
+++ test/projectile-test.el
@@ -341,7 +341,7 @@ Just delegates OPERATION and ARGS for all operations except for`shell-command`'.
 
 (describe "projectile-ignored-directory-p"
   (it "checks if directory should be ignored"
-    (spy-on 'projectile-ignored-directories :and-return-value '("/path/to/project/tmp" "/path/to/project/t\\.*"))
+    (spy-on 'projectile-ignored-directories :and-return-value '("/path/to/project/tmp" "/path/to/project/t.*"))
     (expect (projectile-ignored-directory-p "/path/to/project/tmp") :to-be-truthy)
     (expect (projectile-ignored-directory-p "/path/to/project/t.ignore") :to-be-truthy)
     (expect (projectile-ignored-directory-p "/path/to/project/log") :not :to-be-truthy)))
@@ -1816,12 +1816,12 @@ Just delegates OPERATION and ARGS for all operations except for`shell-command`'.
     (expect 'projectile-discover-projects-in-search-path :to-have-been-called)))
 
 (describe "projectile-test-ignored-directory-p"
-  (it "ignores specified literal directory values"
+  (it "ignores specified regular directory values"
     (spy-on 'projectile-ignored-directories :and-return-value '("/path/to/project/tmp"))
     (expect (projectile-ignored-directory-p "/path/to/project/tmp") :to-be-truthy)
     (expect (projectile-ignored-directory-p "/path/to/project/log") :not :to-be-truthy))
-  (it "ignores specified regex directory values"
-    (spy-on 'projectile-ignored-directories :and-return-value '("/path/to/project/t\\.*"))
+  (it "ignores specified directory values with characters that need to be escaped"
+    (spy-on 'projectile-ignored-directories :and-return-value '("/path/to/project/t.*"))
     (expect (projectile-ignored-directory-p "/path/to/project/tmp") :to-be-truthy)
     (expect (projectile-ignored-directory-p "/path/to/project/log") :not :to-be-truthy)))
 
