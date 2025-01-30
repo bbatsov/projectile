55894d2a009ff51fb2a77a0f6dba4160b39fa24d
Zach Fontenot <60191814+ZachFontenot@users.noreply.github.com>
2025-01-30 00:21:43 -0600

    Update Bloop project config (#1901)
    This solves a problem when you have a `.bloop` file in your home directory, which is a pretty common setup.

--- projectile.el
+++ projectile.el
@@ -3489,6 +3489,12 @@ a manual COMMAND-TYPE command is created with
                                   :test "mill __.test"
                                   :test-suffix "Test")
 
+(projectile-register-project-type 'bloop '(".bloop/bloop.settings.json")                                          :compile "bloop compile root"
+                                  :test "bloop test --propagate --reporter scalac root"
+                                  :src-dir "src/main/"
+                                  :test-dir "src/test/"
+                                  :test-suffix "Spec")
+
 ;; Clojure
 (projectile-register-project-type 'lein-test '("project.clj")
                                   :project-file "project.clj"
@@ -3508,13 +3514,7 @@ a manual COMMAND-TYPE command is created with
 (projectile-register-project-type 'clojure-cli '("deps.edn")
                                   :project-file "deps.edn"
                                   :test-suffix "_test")
-(projectile-register-project-type 'bloop '(".bloop")
-                                  :project-file ".bloop"
-                                  :compile "bloop compile root"
-                                  :test "bloop test --propagate --reporter scalac root"
-                                  :src-dir "src/main/"
-                                  :test-dir "src/test/"
-                                  :test-suffix "Spec")
+
 ;; Ruby
 (projectile-register-project-type 'ruby-rspec '("Gemfile" "lib" "spec")
                                   :project-file "Gemfile"
